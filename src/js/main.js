fad6499b154e5b59ed8a685fe35a38dc68caea9c
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2021-02-26 17:07:20 +0700

    first upload

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+});
+
+var swiper = new Swiper('.swiper-container-testi', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
