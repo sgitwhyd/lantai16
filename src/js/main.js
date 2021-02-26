9d930b29eff9cbdf5920756ebb6ef5cfb720ae19
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2021-02-26 19:13:50 +0700

    fix: response

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 2,
     spaceBetween: 10,
+    breakpoints: {
+        285: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        }
+    }
 });
 
 var swiper = new Swiper('.swiper-container-testi', {
